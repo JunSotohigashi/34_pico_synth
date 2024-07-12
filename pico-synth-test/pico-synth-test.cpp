df49bebccab47933bfee5cc2d4a96b6f1b1048c1
Sotohi <c6h4ohcooc6h5@gmail.com>
2024-07-12 18:06:23 +0900

    ステレオ化

--- pico-synth-test/pico-synth-test.cpp
+++ pico-synth-test/pico-synth-test.cpp
@@ -7,11 +7,12 @@
 #include "oscillator.hpp"
 
 // 定数宣言
-#define PIN_LED 25
-#define PIN_OUT 15
+#define PIN_OUT_L 14
+#define PIN_OUT_R 15
 
 // グローバル変数
-queue_t sound_buffer;
+queue_t sound_buffer_L;
+queue_t sound_buffer_R;
 
 // 関数プロトタイプ宣言
 int main();
@@ -25,21 +26,29 @@ int main()
     stdio_init_all();
 
     // オンボードLED
-    gpio_init(PIN_LED);
-    gpio_set_dir(PIN_LED, GPIO_OUT);
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     // 出力音声のバッファー
-    queue_init(&sound_buffer, sizeof(int16_t), 256);
+    queue_init(&sound_buffer_L, sizeof(int16_t), 16);
+    queue_init(&sound_buffer_R, sizeof(int16_t), 16);
 
     // PWM音声出力 約61kHz
-    gpio_init(PIN_OUT);
-    gpio_set_dir(PIN_OUT, GPIO_OUT);
-    gpio_set_function(PIN_OUT, GPIO_FUNC_PWM);
-    uint pwm_slice_num = pwm_gpio_to_slice_num(PIN_OUT);
-    uint pwm_chan_num = pwm_gpio_to_channel(PIN_OUT);
-    pwm_set_clkdiv_int_frac(pwm_slice_num, 0b1, 0b0);
-    pwm_set_wrap(pwm_slice_num, 2048);
-    pwm_set_enabled(pwm_slice_num, true);
+    gpio_init(PIN_OUT_L);
+    gpio_init(PIN_OUT_R);
+    gpio_set_dir(PIN_OUT_L, GPIO_OUT);
+    gpio_set_dir(PIN_OUT_R, GPIO_OUT);
+    gpio_set_function(PIN_OUT_L, GPIO_FUNC_PWM);
+    gpio_set_function(PIN_OUT_R, GPIO_FUNC_PWM);
+    uint pwm_slice_L = pwm_gpio_to_slice_num(PIN_OUT_L);
+    uint pwm_slice_R = pwm_gpio_to_slice_num(PIN_OUT_R);
+    uint pwm_chan_L = pwm_gpio_to_channel(PIN_OUT_L);
+    uint pwm_chan_R = pwm_gpio_to_channel(PIN_OUT_R);
+    pwm_set_clkdiv_int_frac(pwm_slice_L, 0b1, 0b0);
+    pwm_set_clkdiv_int_frac(pwm_slice_R, 0b1, 0b0);
+    pwm_set_wrap(pwm_slice_L, 2048);
+    pwm_set_wrap(pwm_slice_R, 2048);
+    pwm_set_mask_enabled((1 << pwm_slice_L) | (1 << pwm_slice_R));
 
     // 処理開始
     multicore_launch_core1(main_core1);
@@ -97,12 +106,14 @@ void main_core0()
                 }
             }
             osc1.set_freq_note_number(note);
-            osc2.set_freq_note_number((note+4)%128);
+            osc2.set_freq_note_number((note + 4) % 128);
         }
         int16_t out_level_1 = osc1.get_out_level() / 8;
         int16_t out_level_2 = osc2.get_out_level() / 8;
-        int16_t out_level = out_level_1 + out_level_2;
-        queue_add_blocking(&sound_buffer, &out_level);
+        int16_t out_level_L = out_level_1;
+        int16_t out_level_R = out_level_2;
+        queue_add_blocking(&sound_buffer_L, &out_level_L);
+        queue_add_blocking(&sound_buffer_R, &out_level_R);
     }
 }
 
@@ -117,20 +128,24 @@ void main_core1()
 bool timer_callback(repeating_timer_t *rt)
 {
     // 音声出力バッファが空
-    if (queue_is_empty(&sound_buffer))
+    if (queue_is_empty(&sound_buffer_L) || queue_is_empty(&sound_buffer_R))
     {
-        gpio_put(PIN_LED, true);
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
         return true;
     }
 
     // バッファからの取り出し
-    int16_t out_level;
-    queue_try_remove(&sound_buffer, &out_level);
-    gpio_put(PIN_LED, false);
+    int16_t out_level_L;
+    int16_t out_level_R;
+    queue_try_remove(&sound_buffer_L, &out_level_L);
+    queue_try_remove(&sound_buffer_R, &out_level_R);
+    gpio_put(PICO_DEFAULT_LED_PIN, false);
 
     // -32768~32767(int16) から PWMのデューティー 0~2047(uint11) へ変換
-    uint16_t out_duty = (out_level >> 5) + 1024;
-    pwm_set_gpio_level(PIN_OUT, out_duty);
+    uint16_t out_duty_L = (out_level_L >> 5) + 1024;
+    uint16_t out_duty_R = (out_level_R >> 5) + 1024;
+    pwm_set_gpio_level(PIN_OUT_L, out_duty_L);
+    pwm_set_gpio_level(PIN_OUT_R, out_duty_R);
 
     return true;
 }
