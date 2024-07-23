014b74b5f772c072704d5472d04195642de0150e
Sotohi <c6h4ohcooc6h5@gmail.com>
2024-07-23 17:25:19 +0900

    multicore

--- pico-synth-panel/pico-synth-panel.cpp
+++ pico-synth-panel/pico-synth-panel.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include "hardware/spi.h"
+#include "selector.hpp"
 #include "spi_mcp3008.hpp"
-#include "spi_74hc595.hpp"
 
 // SPI Defines
 #define SPI_PORT_PANEL spi0
@@ -37,102 +38,73 @@
 #define PIN_SW_LFO_TARGET_R 21
 #define PIN_SW_LFO_TARGET_L 20
 
-// void print_adc(SPIMCP3008 adc1, SPIMCP3008 adc2)
-// {
-//     for (uint8_t i = 0; i < 8; i++)
-//     {
-//         printf(">CHa%u: %4u\n", i, adc1.read(i));
-//     }
-//     for (uint8_t i = 0; i < 8; i++)
-//     {
-//         printf(">CHb%u: %4u\n", i, adc2.read(i));
-//     }
-// }
+semaphore_t sem;
+uint16_t unit_state = 0;
+uint8_t unit_note[16] = {0};
+uint8_t unit_velocity[16] = {0};
 
-// void print_sw()
-// {
-//     printf(">PIN_SW_VCO1_L: %d\n", !gpio_get(PIN_SW_VCO1_L));
-//     printf(">PIN_SW_VCO1_R: %d\n", !gpio_get(PIN_SW_VCO1_R));
-//     printf(">PIN_SW_VCO2_L: %d\n", !gpio_get(PIN_SW_VCO2_L));
-//     printf(">PIN_SW_VCO2_R: %d\n", !gpio_get(PIN_SW_VCO2_R));
-//     printf(">PIN_SW_VCF_L: %d\n", !gpio_get(PIN_SW_VCF_L));
-//     printf(">PIN_SW_VCF_R: %d\n", !gpio_get(PIN_SW_VCF_R));
-//     printf(">PIN_SW_LFO_WAVE_L: %d\n", !gpio_get(PIN_SW_LFO_WAVE_L));
-//     printf(">PIN_SW_LFO_WAVE_R: %d\n", !gpio_get(PIN_SW_LFO_WAVE_R));
-//     printf(">PIN_SW_LFO_TARGET_L: %d\n", !gpio_get(PIN_SW_LFO_TARGET_L));
-//     printf(">PIN_SW_LFO_TARGET_R: %d\n", !gpio_get(PIN_SW_LFO_TARGET_R));
-// }
-
-class Selector
+void main_core1()
 {
-public:
-    Selector(uint pin_sw_r, uint pin_sw_l, uint8_t n_state, SPI74HC595 **led_sr, uint8_t *led_pin)
-        : pin_sw_r(pin_sw_r),
-          pin_sw_l(pin_sw_l),
-          sw_r_old(false),
-          sw_l_old(false),
-          n_state(n_state),
-          state(0),
-          led_sr(led_sr),
-          led_pin(led_pin)
+    char buf[256] = {0};
+    char buf_msg[12] = {0};
+    uint8_t buf_index = 0;
+    uint8_t unit_now = 0;
+    while (true)
     {
-        gpio_init(pin_sw_r);
-        gpio_init(pin_sw_l);
-        gpio_set_dir(pin_sw_r, GPIO_IN);
-        gpio_set_dir(pin_sw_l, GPIO_IN);
-        gpio_set_pulls(pin_sw_r, true, false);
-        gpio_set_pulls(pin_sw_l, true, false);
-        gpio_set_input_hysteresis_enabled(pin_sw_r, true);
-        gpio_set_input_hysteresis_enabled(pin_sw_l, true);
-        show_leds();
-    };
-
-    void update()
-    {
-        bool sw_r = !gpio_get(pin_sw_r);
-        bool sw_l = !gpio_get(pin_sw_l);
-        if ((sw_r && !sw_r_old) || (sw_l && !sw_l_old))
+        if (uart_is_readable(UART_PORT))
         {
-            if (sw_r && !sw_r_old)
-                state = (state + 1) % n_state;
-            else
-                state = (state + n_state - 1) % n_state;
-            show_leds();
-        }
-        sw_r_old = sw_r;
-        sw_l_old = sw_l;
-    }
-
-    uint8_t get_state()
-    {
-        return state;
-    }
-
-private:
-    uint pin_sw_r;
-    uint pin_sw_l;
-    bool sw_r_old;
-    bool sw_l_old;
-    uint8_t n_state;
-    uint8_t state;
-    SPI74HC595 **led_sr;
-    uint8_t *led_pin;
-    void show_leds()
-    {
-        for (uint8_t i = 0; i < n_state; i++)
-        {
-            led_sr[i]->put(led_pin[i], state == i);
+            buf[buf_index] = uart_getc(UART_PORT);
+            // message end
+            if (buf[buf_index] == '\n')
+            {
+                for (uint16_t i = 0; i <= 8; i++)
+                {
+                    buf_msg[i] = buf[(buf_index + i - 8) & 0xFF];
+                }
+                buf_msg[10] = '\0';
+                uint8_t msg[3];
+                sscanf(buf_msg, "%hhx %hhx %hhx", &msg[0], &msg[1], &msg[2]);
+                // assign unit
+                if (msg[0] == 0x90)
+                {
+                    sem_acquire_blocking(&sem);
+                    for (uint8_t i = 0; i < 16; i++)
+                    {
+                        uint8_t i_offset = (i + unit_now) & 0x0F;
+                        if (msg[2] == 0) // note off
+                        {
+                            if ((unit_state & 1 << i) && unit_note[i] == msg[1])
+                            {
+                                unit_note[i] = 0;
+                                unit_velocity[i] = 0;
+                                unit_state &= ~(1 << i);
+                                break;
+                            }
+                        }
+                        else // note on
+                        {
+                            if (!(unit_state & 1 << i_offset))
+                            {
+                                unit_state |= 1 << i_offset;
+                                unit_note[i_offset] = msg[1];
+                                unit_velocity[i_offset] = msg[2];
+                                unit_now = (i_offset + 1) & 0x0F;
+                                break;
+                            }
+                        }
+                    }
+                    sem_release(&sem);
+                }
+            }
+            buf_index++;
         }
     }
-};
-
-
-
-
+}
 
 int main()
 {
     stdio_init_all();
+    sem_init(&sem, 1, 1);
 
     // UART
     uart_init(UART_PORT, 115200);
@@ -189,14 +161,7 @@ int main()
     uint8_t lfo_target_pin[6] = {1, 2, 3, 4, 5, 6};
     Selector sel_lfo_target(PIN_SW_LFO_TARGET_R, PIN_SW_LFO_TARGET_L, 6, lfo_target_sr, lfo_target_pin);
 
-    uint8_t unit_now = 0;
-    uint16_t unit_state = 0;
-    uint8_t unit_note[16] = {0};
-    uint8_t unit_velocity[16] = {0};
-
-    char buf[256] = {0};
-    char buf_msg[12] = {0};
-    uint8_t buf_index = 0;
+    multicore_launch_core1(main_core1);
 
     while (true)
     {
@@ -206,61 +171,17 @@ int main()
         sel_lfo_wave.update();
         sel_lfo_target.update();
 
-        if (uart_is_readable(UART_PORT))
-        {
-            buf[buf_index] = uart_getc(UART_PORT);
-            // message end
-            if (buf[buf_index] == '\n')
-            {
-                for (uint16_t i = 0; i <= 8; i++)
-                {
-                    buf_msg[i] = buf[(buf_index + i - 8) & 0xFF];
-                }
-                buf_msg[10] = '\0';
-                uint8_t msg[3];
-                sscanf(buf_msg, "%hhx %hhx %hhx", &msg[0], &msg[1], &msg[2]);
-                // printf("%02x %02x %02x\n", msg[0], msg[1], msg[2]);
-                // assign unit
-                if (msg[0] == 0x90)
-                {
-                    for (uint8_t i = 0; i < 16; i++)
-                    {
-                        uint8_t i_offset = (i + unit_now) & 0x0F;
-                        if (msg[2] == 0) // note off
-                        {
-                            if ((unit_state & 1 << i) && unit_note[i] == msg[1])
-                            {
-                                unit_note[i] = 0;
-                                unit_velocity[i] = 0;
-                                unit_state &= ~(1 << i);
-                                break;
-                            }
-                        }
-                        else // note on
-                        {
-                            if (!(unit_state & 1 << i_offset))
-                            {
-                                unit_state |= 1 << i_offset;
-                                unit_note[i_offset] = msg[1];
-                                unit_velocity[i_offset] = msg[2];
-                                unit_now = (i_offset + 1) & 0x0F;
-                                break;
-                            }
-                        }
-                    }
-                    led_unit_high.put_8bit((unit_state & 0xFF) << 1 | (unit_state & 0xFF) >> 7);
-                    led_unit_low.put_8bit((unit_state >> 8 & 0xFF) << 1 | (unit_state >> 8 & 0xFF) >> 7);
-                }
-            }
-            buf_index++;
-        }
+        led_unit_high.put_8bit((unit_state & 0xFF) << 1 | (unit_state & 0xFF) >> 7);
+        led_unit_low.put_8bit((unit_state >> 8 & 0xFF) << 1 | (unit_state >> 8 & 0xFF) >> 7);
 
         uint8_t stream[54];
+        sem_acquire_blocking(&sem);
         for (uint8_t ch = 0; ch < 16; ch++)
         {
             stream[ch * 2] = ((unit_state >> ch) & 1) << 7 | unit_note[ch];
             stream[ch * 2 + 1] = unit_velocity[ch];
         }
+        sem_release(&sem);
         uint16_t duty = adc1.read(2);
         uint16_t tune2 = adc1.read(3);
         uint16_t mix = adc1.read(1);
@@ -299,7 +220,7 @@ int main()
         stream[51] = lfo_speed;
         stream[52] = sel_lfo_wave.get_state() << 5 | sel_lfo_target.get_state() << 2 | lfo_depth >> 8;
         stream[53] = lfo_depth;
-        for(uint8_t i = 0; i < 53; i++)
+        for (uint8_t i = 0; i < 53; i++)
         {
             printf("%02x", stream[i]);
         }
---
+++ pico-synth-panel/selector.hpp
@@ -0,0 +1,70 @@
+#ifndef SELECTOR_HPP
+#define SELECTOR_HPP
+
+#include "pico/stdlib.h"
+#include "spi_74hc595.hpp"
+
+class Selector
+{
+public:
+    Selector(uint pin_sw_r, uint pin_sw_l, uint8_t n_state, SPI74HC595 **led_sr, uint8_t *led_pin)
+        : pin_sw_r(pin_sw_r),
+          pin_sw_l(pin_sw_l),
+          sw_r_old(false),
+          sw_l_old(false),
+          n_state(n_state),
+          state(0),
+          led_sr(led_sr),
+          led_pin(led_pin)
+    {
+        gpio_init(pin_sw_r);
+        gpio_init(pin_sw_l);
+        gpio_set_dir(pin_sw_r, GPIO_IN);
+        gpio_set_dir(pin_sw_l, GPIO_IN);
+        gpio_set_pulls(pin_sw_r, true, false);
+        gpio_set_pulls(pin_sw_l, true, false);
+        gpio_set_input_hysteresis_enabled(pin_sw_r, true);
+        gpio_set_input_hysteresis_enabled(pin_sw_l, true);
+        show_leds();
+    };
+
+    void update()
+    {
+        bool sw_r = !gpio_get(pin_sw_r);
+        bool sw_l = !gpio_get(pin_sw_l);
+        if ((sw_r && !sw_r_old) || (sw_l && !sw_l_old))
+        {
+            if (sw_r && !sw_r_old)
+                state = (state + 1) % n_state;
+            else
+                state = (state + n_state - 1) % n_state;
+            show_leds();
+        }
+        sw_r_old = sw_r;
+        sw_l_old = sw_l;
+    }
+
+    uint8_t get_state()
+    {
+        return state;
+    }
+
+private:
+    uint pin_sw_r;
+    uint pin_sw_l;
+    bool sw_r_old;
+    bool sw_l_old;
+    uint8_t n_state;
+    uint8_t state;
+    SPI74HC595 **led_sr;
+    uint8_t *led_pin;
+    void show_leds()
+    {
+        for (uint8_t i = 0; i < n_state; i++)
+        {
+            led_sr[i]->put(led_pin[i], state == i);
+        }
+    }
+};
+
+#endif
